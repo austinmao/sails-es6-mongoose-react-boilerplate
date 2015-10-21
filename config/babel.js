fa472791ca75c7209f254e729d227365fb907348
Austin Mao <austin@austinmao.com>
2015-10-21 11:11:16 -0700

    Adding babel config

---
+++ config/babel.js
@@ -0,0 +1,23 @@
+/**
+ * Babel configuration
+ * (sails.config.babel)
+ *
+ * @see: https://github.com/artificialio/sails-hook-babel
+ * Parameter Type  Details
+ * compile ((boolean)) Whether or not sails should compile future JS code. Defaults to true.
+ * stage ((integer)) Which stage of Proposals to use 0 being the most experimental. Defaults to 2.
+ * loose ((boolean)) Whether or not use babel's loose mode. Defaults to true.
+ * ignore  ((boolean/Regex)) Can be false or a regex of what to ignore. For default mode see http://babeljs.io/docs/usage/require/
+ * only  ((Regex)) Whether or not use babel's loose mode. For default mode see http://babeljs.io/docs/usage/require/
+ * extensions  ((array)) Pass an array of extensions, such as ['.js', '.es']. For default mode see http://babeljs.io/docs/usage/require/
+ */
+
+
+module.exports.babel = {
+  // compile: true,
+  // stage: true,
+  // loose: true,
+  // ignore: false,
+  // only: '',
+  // extensions: ''
+}
