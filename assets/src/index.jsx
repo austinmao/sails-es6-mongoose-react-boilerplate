1c1198fd6489e71fcd7625ab7391b60a18443bf7
Austin Mao <austin@austinmao.com>
2015-11-02 14:23:40 -0800

    Adding components

---
+++ assets/src/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router, {Route} from 'react-router';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import io from 'socket.io-client';
+import reducer from './reducer';
+import {setClientId, setState, setConnectionState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
+import getClientId from './client_id';
+import App from './components/App';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
+
+require('./style.css');
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch(setState(state))
+);
+[
+  'connect',
+  'connect_error',
+  'connect_timeout',
+  'reconnect',
+  'reconnecting',
+  'reconnect_error',
+  'reconnect_failed'
+].forEach(ev =>
+  socket.on(ev, () => store.dispatch(setConnectionState(ev, socket.connected)))
+);
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
+store.dispatch(setClientId(getClientId()));
+
+const routes = <Route component={App}>
+  <Route path="/" component={VotingContainer} />
+  <Route path="/results" component={ResultsContainer} />
+</Route>;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
+  document.getElementById('app')
+);
