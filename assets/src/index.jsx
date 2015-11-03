47be0cc1bb77dcdd6eb0e479600874485600f327
Austin Mao <austin@austinmao.com>
2015-11-02 16:20:55 -0800

    Webpack loaded with react

--- assets/src/index.jsx
+++ assets/src/index.jsx
@@ -12,7 +12,7 @@ import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
-require('./style.css');
+// require('../styles/style.css');
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+var path = require('path')
+
+module.exports = {
+  entry: [
+    // 'webpack-dev-server/client?http://localhost:8080',
+    // 'webpack/hot/only-dev-server',
+    // './assets/src/index.jsx'
+    './assets/src/index.jsx'
+  ],
+  resolveLoader: {
+    fallback: [path.join(__dirname, 'node_modules')],
+    modulesDirectories: [path.join(__dirname, "node_modules")],
+    root: path.join(__dirname, 'node_modules')
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loaders: ['react-hot', 'babel'],
+      // include: __dirname + '/assets/src',
+    }, {
+      test: /\.css$/,
+      loader: 'style!css!autoprefixer?browsers=last 2 versions'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    fallback: path.join(__dirname, 'node_modules')
+  },
+  output: {
+    path: __dirname + '/assets/js',
+    // publicPath: 'http://localhost:8080/build/',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './assets/js',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
